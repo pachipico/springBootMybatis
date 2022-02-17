ed490d3b1aca74648d5bbe9605bafe5e302ae81f
pachipico <pachipico1@gmail.com>
2022-02-17 17:04:27 +0900

    initial commit

---
+++ src/main/java/com/myweb/board/dto/BoardDTO.java
@@ -0,0 +1,8 @@
+package com.myweb.board.dto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BoardDTO {
+
+}
---
+++ src/main/resources/static/dist/schema.sql
@@ -0,0 +1,10 @@
+create table pboard (
+    pno bigint(20) not null auto_increment,
+    title varchar(200) not null,
+    writer varchar(100) not null,
+    content text,
+    reg_at datetime not null default (now()),
+    mod_at datetime not null default (now()),
+    readcount bigint(20) default 0,
+    primary key (pno)
+);
